8a0e96a09560e82cbeb0dc5b9b23f2825651da61
KoshuVine <koshuvine@gmail.com>
2025-01-23 16:03:57 -0700

    saved

--- signal_bus.gd
+++ signal_bus.gd
@@ -1,6 +1,10 @@
 extends Node
 
+@warning_ignore("unused_signal")
 signal keep_item
+@warning_ignore("unused_signal")
 signal discard_item
+@warning_ignore("unused_signal")
 signal open_checklist
+@warning_ignore("unused_signal")
 signal open_inventory
