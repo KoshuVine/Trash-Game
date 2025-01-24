d91c4614394a47c04c657ce7473ecc9dd60dee9b
KoshuVine <koshuvine@gmail.com>
2025-01-24 15:15:28 -0700

    Added a function to calculate sanity based on 4 stats the player can have.

--- stats.gd
+++ stats.gd
@@ -1,5 +1,11 @@
 extends Node
 
-var sanity = 100
+var happiness = 100
 var hunger = 100
 var hygiene = 100
+var wealth = 100
+
+
+func calculate_sanity():
+	var sanity = (happiness + hunger + hygiene + wealth) / 4
+	return sanity
