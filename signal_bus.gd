63a758fd0aafce29381641962caf283c8b382717
KoshuVine <koshuvine@gmail.com>
2025-01-24 21:22:08 -0700

    Added signals to detect stat changes

--- signal_bus.gd
+++ signal_bus.gd
@@ -8,3 +8,11 @@ signal discard_item
 signal open_checklist
 @warning_ignore("unused_signal")
 signal open_inventory
+@warning_ignore("unused_signal")
+signal update_health
+@warning_ignore("unused_signal")
+signal update_hygiene
+@warning_ignore("unused_signal")
+signal update_wealth
+@warning_ignore("unused_signal")
+signal update_hunger
