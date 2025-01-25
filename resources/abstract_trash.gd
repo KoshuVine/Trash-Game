750d72c33354d276db62e10cff964b08d9847b29
KoshuVine <koshuvine@gmail.com>
2025-01-24 22:46:54 -0700

    Added full description to AbstractTrash to_string

--- resources/abstract_trash.gd
+++ resources/abstract_trash.gd
@@ -4,18 +4,41 @@ class_name AbstractTrash extends Node
 @export var trash_name: String
 #@export var texture: AnimatedSprite2D
 @export var quality: int
-func _init(item_name:String = "Unnamed", item_quality = 0):
+enum Cleanliness {
+	NOT_APPLICABLE = 0,
+	VERY_CLEAN = 1,
+	CLEAN = 2,
+	NEUTRAL = 3,
+	DIRTY = 4,
+	VERY_DIRTY = 5,
+}
+enum Nutrition {
+	NOT_APPLICABLE = 0,
+	VERY_NUTRITIOUS = 1,
+	NUTRITIOUS = 2,
+	NEUTRAL = 3,
+	DANGEROUS = 4,
+	POISONOUS = 5,
+}
+enum Safety {
+	NOT_APPLICABLE = 0,
+	VERY_HELPFUL = 1,
+	HELPFUL = 2,
+	NEUTRAL = 3,
+	UNSAFE = 4,
+	TOXIC = 5,
+}
+@export var cleanliness_value : Cleanliness
+@export var nutrition_value : Nutrition
+@export var safety_value : Safety
+
+func _init(item_name:String = "Unnamed", item_quality = 0, cleanliness : Cleanliness = 0, nutrition : Nutrition = 0, safety : Safety = 0):
 	#texture = item_texture
 	quality = item_quality
 	trash_name = item_name
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	cleanliness_value = cleanliness
+	nutrition_value = nutrition
+	safety_value = safety
 
 func _to_string():
-	return trash_name
+	return ("\n Name: " + trash_name + "\n quality: " + str(quality) + "\n Cleanliness: " + Cleanliness.keys()[cleanliness_value] + "\n Nutrition: " + Nutrition.keys()[nutrition_value] + "\n Safety: " + Safety.keys()[safety_value] + "\n")
